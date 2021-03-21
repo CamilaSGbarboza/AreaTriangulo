09329750bc232374c1045fa48d62659fb4ea3a74
camila <camilasangobar@gmail.com>
2021-03-21 19:30:17 -0300

    Area

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AreaTriangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("---Área do triangulo---");
+            Console.WriteLine();
+            Console.ResetColor();
+            Console.WriteLine("Base:");
+            double b = Convert.ToDouble (Console.ReadLine());
+            Console.WriteLine("Altura:");
+            double h = Convert.ToDouble(Console.ReadLine());
+            double a = b * h/2;
+            Console.WriteLine();
+            Console.WriteLine($"Área:{a}");
+            
+        }
+    }
+}
